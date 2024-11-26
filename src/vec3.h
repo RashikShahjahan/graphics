62bfeca7d9450fe776f1e8612c2e012538693da0
Rashik Shahjahan <rashikshahjahan@protonmail.com>
2024-11-25 22:59:16 -0500

    color

--- src/vec3.h
+++ src/vec3.h
@@ -8,8 +8,8 @@ class vec3 {
     public:
         float e[3];
 
-        vec3(): e(0,0,0){};
-        vec3(float e1,float e2,float e3): e(e1,e2,e3){};
+        vec3(): e{0,0,0}{}
+        vec3(float e0,float e1,float e2): e{e0,e1,e2}{}
 
         float x() const {return e[0];}
         float y() const {return e[1];}
@@ -17,21 +17,21 @@ class vec3 {
 
         vec3 operator-() const{return vec3(-e[0],-e[1],-e[2]);}
         float operator[] (int i) const{return e[i];}
-        &float operator[] (int i) {return e[i];}
+        float& operator[] (int i) {return e[i];}
 
-        vec3 operator+= (const vec3 &v) {
+        vec3& operator+= (const vec3 v) {
             e[0] += v.e[0];
             e[1] += v.e[1];
             e[2] += v.e[2];
             return *this;
             }
-        vec3 operator*= (float t) {
+        vec3& operator*= (float t) {
             e[0] *= t;
             e[1] *= t;
             e[2]*= t;
             return *this;
         }
-        vec3 operator/= (float t) {
+        vec3& operator/= (float t) {
             return *this*=1/t;
 
         }
@@ -48,8 +48,8 @@ class vec3 {
 using point3 = vec3;
 
 inline std::ostream& operator<<(std::ostream& out, vec3& v ){
-    return out << v.e[0] << ' ' v.e[1] << ' ' << v.e[2];
-}
+    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+    }
 
 inline vec3 operator+(vec3& u ,vec3& v ){
     return vec3(u.e[0]+v.e[0], u.e[1]+v.e[1], u.e[2]+v.e[2]);
@@ -63,12 +63,12 @@ inline vec3 operator*(vec3& u ,vec3& v ){
     return vec3(u.e[0]*v.e[0], u.e[1]*v.e[1], u.e[2]*v.e[2]);
 }
 
-inline vec3 operator*(vec3& v ,float t ){
+inline vec3 operator*(const vec3& v ,float t ){
     return v*t;
 }
 
-inline vec3 operator/(vec3& v ,float t ){
-    return v/t;
+inline vec3 operator/(const vec3& v ,float t ){
+    return v*(1/t);
 }
 
 inline float dot(vec3& u ,vec3& v){
